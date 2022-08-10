42869c9686df061e6a6439e8220cdf6668ab6c18
Xinjie Qiu <qiuxinjie@gmail.com>
2022-08-10 00:21:17 -0400

    Add a new page: HCP engagement

--- Pharma_Data_Science_Demo.py
+++ Pharma_Data_Science_Demo.py
@@ -23,47 +23,42 @@ def run():
 
     mcol1, mcol2, mcol3 = st.columns(3)
     with mcol1:
+        st.markdown("""##### High Value HCP""")
+        st.image("figures/hcp_network.png", width=180)
+
+    with mcol2:
+        st.markdown("""##### HCP Engagement""")
+        st.image("figures/HCP.png", width=180)
+
+    with mcol3:
+        st.markdown("""##### Channel optimization""")
+        st.image("figures/channel.png", width=200)
+
+
+    mcol1, mcol2, mcol3 = st.columns(3)
+    with mcol1:
         st.markdown("""##### Market share trend""")
         st.image("figures/marketshare.png", width=160)
 
     with mcol2:
-        st.markdown("""##### High Value HCP""")
-        st.image("figures/hcp_network.png", width=170)
-
-    with mcol3:
         st.markdown("""##### Line of therapies""")
         st.image("figures/LOT.png", width=180)
 
-    mcol1, mcol2, mcol3 = st.columns(3)
+    with mcol3:
+        st.markdown("""##### Medication persistence""")
+        st.image("figures/persistence.png", width=200)
+
+
+    space0, mcol1, space1, mcol2, space2, = st.columns([1,2,.5,2,1])
     with mcol1:
         st.markdown("""##### Diagnosis journey""")
         st.image("figures/journey_diagnosis.png", width=180)
 
-
     with mcol2:
-        st.markdown("""##### Channel optimization""")
-        st.image("figures/channel.png", width=200)
-
-    with mcol3:
         st.markdown("""##### Treatment journey""")
         st.image("figures/journey_treatment.png", width=180)
 
-    mcol1, mcol2, mcol3 = st.columns(3)
-    # with mcol1:
-        # st.markdown("""##### Line of therapies""")
-        # st.image("figures/LOT.png", width=180)
-
-    with mcol2:
-        st.markdown("""##### Medication persistence""")
-        st.image("figures/persistence.png", width=200)
-
-    # with mcol3:
-    #     st.markdown("""##### Diagnosis journey""")
-    #     st.image("figures/journey_diagnosis.png", width=180)
-
     st.markdown(""" ###### © 2022 Xinjie Qiu ℠""")
-
-
 
 if __name__ == "__main__":
     run()
